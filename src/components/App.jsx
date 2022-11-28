c204d039305490695c06bd845d4f7b9a2ea4b0c3
Ivan-Prystay <prystay.ivan@gmail.com>
2022-11-28 22:30:43 +0200

    task 2 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import user from 'path/user.json';
+import data from 'path/data.json';
 
 import FriendList from './FriendList/FriendList';
 import Profile from './Profile/Profile';
@@ -15,8 +16,19 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics
+        title="Upload stats"
+        label={data[0].label}
+        percentage={data[0].percentage}
+      />
+      <Statistics
+        title="Upload stats"
+        label={data[1].label}
+        percentage={data[1].percentage}
+      />
+
       <FriendList />
-      <Statistics />
       <TransactionHistory />
     </div>
   );
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,1 +1,28 @@
-export default function Statistics(params) {}
+//import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+//import data from 'path/data.json';
+
+/*
+[
+  { "id": "id-1", "label": ".docx", "percentage": 22 },
+  { "id": "id-2", "label": ".pdf", "percentage": 4 },
+  { "id": "id-3", "label": ".mp3", "percentage": 17 },
+  { "id": "id-4", "label": ".psd", "percentage": 47 },
+  { "id": "id-5", "label": ".pdf", "percentage": 10 }
+]
+*/
+
+export default function Statistics({ title, label, percentage }) {
+  return (
+    <section className={css.statistics}>
+      <h2 className="title">{title}</h2>
+
+      <ul className="stat-list">
+        <li className="item">
+          <span className="label">{label}</span>
+          <span className="percentage">{percentage}%</span>
+        </li>
+      </ul>
+    </section>
+  );
+}

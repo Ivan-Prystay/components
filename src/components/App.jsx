972f102eda21cf1f76987ace05378b8aa21dd766
Ivan-Prystay <prystay.ivan@gmail.com>
2022-12-03 17:09:33 +0200

    task 4 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import user from 'path/user.json';
 import data from 'path/data.json';
 import friends from 'path/friends.json';
+import transactions from 'path/transactions.json';
 
 import { FriendList } from './FriendList/FriendList';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-//import TransactionHistory from './TransactionHistory/TransactionHistory';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -19,7 +20,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-      {/* <TransactionHistory /> */}
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,1 +1,25 @@
-export default function TransactionHistory(params) {}
+import css from './TransactionHistory.module.css';
+
+export function TransactionHistory({ items }) {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(transaction => (
+          <tr key={transaction.id}>
+            <td>{transaction.type}</td>
+            <td>{transaction.amount}</td>
+            <td>{transaction.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
